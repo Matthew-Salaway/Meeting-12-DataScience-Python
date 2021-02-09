a4484292ade0cff6c3870f6b90716676bdc40c67
Matthew Salaway <matthewsalaway@gmail.com>
2021-02-09 11:45:57 -0700

    Create startingCode.py

---
+++ startingCode.py
@@ -0,0 +1,50 @@
+# Copy this code into your own repl. Click the navigation bar in the top left corner and go to My Repl.
+# Click the New Repl on the left, create the repl, and paste the code. Or put into another IDE.
+
+# Today we will be having a review over the fundamentals of python needed for data science. (and some statistics)
+
+# Basic python is needed for cleaning data, exploratory data analysis, and modeling.
+# It is also needed to collect data if a preset database is not available.
+
+# Looking at the flow chart: the processes from data collection to evaluation will
+# utilize basic python and modules. This will be a lesson on basic python... Modules later!
+
+# Who remembers how to print to the terminal? Try it below!
+
+
+# Now try creating a variable - one of each type (int, double, boolean)
+
+
+
+
+
+# Now print one of the variables in context! (ex. "[color] is my favorite color")
+
+
+
+# There are many ways to modify variables. In data science we have to find how that modification
+# will help when analyzing the data. Most of the time we will modify Ints & Doubles.
+
+# Say we have 3 numbers and want to find the average. Lets first hard code this, then use a function.
+num1 = 67464
+num2 = 44.873
+num3 = -1234
+
+
+
+
+# Now lets create a function that takes three numbers and finds the average.
+
+# Here is a function we created in meeting #9 to help you with syntax.
+def customizedHello(name, age):
+    print("Hello " + name + ". You are age: " + str(age))
+    #customizedHello("Matthew", 18)
+
+# Since repl is not very good at computing in terms of speed, everyone should download an IDE like pycharm
+# We will all code in repl and then copy the code into the IDE.
+
+# We should also quickly go over a few important statistics concepts:
+# https://towardsdatascience.com/the-5-basic-statistics-concepts-data-scientists-need-to-know-2c96740377ae
+
+# Programming requires problem solving. Knowing how to complete an objective is much more
+# important than memorizing how to do it. You can always look up the proper syntax, or how to execute something.
